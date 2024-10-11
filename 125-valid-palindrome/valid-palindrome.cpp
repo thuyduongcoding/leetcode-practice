a8695d960f9d5cdc2dbf6f9136fa4774573fe4a2
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2024-10-11 20:48:36 +0700

    Time: 9 ms (22.23%) | Memory: 8.7 MB (92.59%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            // Store only valid alphanumeric characters
+            if ((s[i] >= 'A' && s[i] <= 'Z') ||   // Uppercase letters
+                (s[i] >= 'a' && s[i] <= 'z') ||   // Lowercase letters
+                (s[i] >= '0' && s[i] <= '9')) {   // Digits
+                s[j] = tolower(s[i]); // Store valid character
+                j++;         // Increment index for valid characters
+            }
+        }
+        // Resize the string to contain only valid characters
+        s.resize(j);
+        cout << s << " ";
+        
+        int first = 0;
+        int last = s.size() - 1;
+
+        while (first < last) {
+            if (s[first] == s[last]) {
+                first++;
+                last--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
