5c4be19c3a699a3ade5f5511ed53c5b8224e0603
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2024-10-11 21:05:51 +0700

    Time: 4 ms (79.84%) | Memory: 8.9 MB (53.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,47 +3,24 @@ public:
     bool isPalindrome(string s) {
         
         // My solution:
-        // int j = 0;
-        // for (int i = 0; i < s.size(); i++) {
-        //     // Store only valid alphanumeric characters
-        //     if ((s[i] >= 'A' && s[i] <= 'Z') ||   // Uppercase letters
-        //         (s[i] >= 'a' && s[i] <= 'z') ||   // Lowercase letters
-        //         (s[i] >= '0' && s[i] <= '9')) {   // Digits
-        //         s[j] = tolower(s[i]); // Store valid character
-        //         j++;         // Increment index for valid characters
-        //     }
-        // }
-        // // Resize the string to contain only valid characters
-        // s.resize(j);
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            // Store only valid alphanumeric characters
+            if ((s[i] >= 'A' && s[i] <= 'Z') ||   // Uppercase letters
+                (s[i] >= 'a' && s[i] <= 'z') ||   // Lowercase letters
+                (s[i] >= '0' && s[i] <= '9')) {   // Digits
+                s[j] = tolower(s[i]); // Store valid character
+                j++;         // Increment index for valid characters
+            }
+        }
+        // Resize the string to contain only valid characters
+        s.resize(j);
         
-        // int start = 0;
-        // int end = s.size() - 1;
-
-        // while (start < end) {
-        //     if (s[start] == s[end]) {
-        //         start++;
-        //         end--;
-        //     } else {
-        //         return false;
-        //     }
-        // return true;
-        // }
-
-
-        // Better solution:
         int start = 0;
         int end = s.size() - 1;
 
         while (start < end) {
-            if (!isalnum(s[start])){
-                start++;
-                continue; // skip the current iteration
-            }
-            if (!isalnum(s[end])) {
-                end--;
-                continue; // skip the current iteration
-            }
-            if (tolower(s[start]) == tolower(s[end])){
+            if (s[start] == s[end]) {
                 start++;
                 end--;
             } else {
@@ -51,5 +28,27 @@ public:
             }
         }
         return true;
+
+    //     // Better solution:
+    //     int start = 0;
+    //     int end = s.size() - 1;
+
+    //     while (start < end) {
+    //         if (!isalnum(s[start])){
+    //             start++;
+    //             continue; // skip the current iteration
+    //         }
+    //         if (!isalnum(s[end])) {
+    //             end--;
+    //             continue; // skip the current iteration
+    //         }
+    //         if (tolower(s[start]) == tolower(s[end])){
+    //             start++;
+    //             end--;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
     }
 };
