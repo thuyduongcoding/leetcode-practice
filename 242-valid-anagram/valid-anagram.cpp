815707d3a0b1f2efa319cd50da436857f7073010
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2024-10-12 21:54:38 +0700

    Time: 7 ms (76.29%) | Memory: 8.9 MB (42.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -12,25 +12,51 @@ public:
         return a_map;
     }
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> s_map = find_occurence(s);
-        unordered_map<char, int> t_map = find_occurence(t);
+        // My solution
+        // unordered_map<char, int> s_map = find_occurence(s);
+        // unordered_map<char, int> t_map = find_occurence(t);
 
-        for (auto& pair : s_map) {
-            if (t_map.find(pair.first) == t_map.end()) {
-                return false;
-            } else if (s_map[pair.first] != t_map[pair.first]) {
+        // for (auto& pair : s_map) {
+        //     if (t_map.find(pair.first) == t_map.end()) {
+        //         return false;
+        //     } else if (s_map[pair.first] != t_map[pair.first]) {
+        //         return false;
+        //     }
+        // }
+
+        // for (auto& pair : t_map) {
+        //     if (s_map.find(pair.first) == s_map.end()) {
+        //         return false;
+        //     } else if (t_map[pair.first] != s_map[pair.first]) {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        // Better solution
+
+        int s_size = s.size();
+        int t_size = t.size();
+
+        vector<int> cnt(26);
+
+        if (s_size!=t_size) {
+            return false;
+        } 
+
+        for(char ele:s) {
+            cnt[ele - 'a']++;
+        }
+        for (char ele:t) {
+            cnt[ele - 'a']--;
+        }
+
+        for(int num : cnt) {
+            if (num!=0) {
                 return false;
             }
         }
-
-        for (auto& pair : t_map) {
-            if (s_map.find(pair.first) == s_map.end()) {
-                return false;
-            } else if (t_map[pair.first] != s_map[pair.first]) {
-                return false;
-            }
-        }
-
         return true;
 
     }
