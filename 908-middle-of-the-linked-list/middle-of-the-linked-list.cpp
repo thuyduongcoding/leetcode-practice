6c1e4a5d92e387893796c3ff847c0f851cc2f2be
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2024-10-17 12:34:18 +0700

    Time: 0 ms (100.00%) | Memory: 9 MB (98.85%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -8,29 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ // 1,2,3,4,5,6,7,8,9 -> 5
+ // 1,2,3,4,5,6 -> 4
+ // 1,2,3,4 -> 3
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int count = countNode(head);
-        ListNode* current = head;
-        if (count %2 != 0){
-            for (int i = 0; i < (count-1)/2; i++) {
-                current = current -> next;
-            }
+        // int count = countNode(head);
+        // ListNode* current = head;
+        ListNode* fastptr = head;
+        ListNode* slowptr = head;
+
+        while (fastptr->next != nullptr && fastptr->next->next !=nullptr){
+            fastptr = fastptr -> next -> next;
+            slowptr = slowptr->next;
+        }
+
+        if (fastptr->next == nullptr){
+            return slowptr;
         } else {
-            for (int i = 0; i < (count)/2; i++) {
-                current = current -> next;
-            }
+            return slowptr->next;
         }
-        return current;
+        // if (count %2 != 0){
+        //     for (int i = 0; i < (count-1)/2; i++) {
+        //         current = current -> next;
+        //     }
+        // } else {
+        //     for (int i = 0; i < (count)/2; i++) {
+        //         current = current -> next;
+        //     }
+        // }
+        // return current;
     }
-    int countNode(ListNode* head) {
-        int count = 0;
-        ListNode* current = head;
-        while(current != nullptr) {
-            count++;
-            current = current -> next; 
-        }
-        return count;
-    }
+    // int countNode(ListNode* head) {
+    //     int count = 0;
+    //     ListNode* current = head;
+    //     while(current != nullptr) {
+    //         count++;
+    //         current = current -> next; 
+    //     }
+    //     return count;
+    // }
 };
