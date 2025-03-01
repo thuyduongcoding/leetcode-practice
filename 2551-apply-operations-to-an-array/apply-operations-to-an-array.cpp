ae47e4b7248e65a6b89410d489b6e3d4a987946c
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-03-01 20:02:01 +0700

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    void swap(int& a, int& b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
     vector<int> applyOperations(vector<int>& nums) {
         vector<int> result;
 
@@ -15,19 +10,9 @@ public:
             } else if (nums[i] != 0) result.push_back(nums[i]);
         }
 
-        while (result.size() != nums.size()) {
+        while (nums.size() != result.size()) {
             result.push_back(0);
         }
-
-        // for (int i = nums.size() - 1; i >= 0; i--) {
-        //     if (nums[i] == 0) {
-        //         int j = i;
-        //         while (j < nums.size() - 1 && nums[j+1] != 0) {
-        //             swap(nums[j], nums[j+1]);
-        //             j++;
-        //         }
-        //     }
-        // }
         return result;
     }
 };
