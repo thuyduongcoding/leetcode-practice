786433122cd4c990da0daeb7fca889c57931123a
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-03-01 20:03:33 +0700

    Time: 0 ms (100.00%) | Memory: 13 MB (87.59%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    vector<int> applyOperations(vector<int>& nums) {
-        vector<int> result;
-
-        for (int i = 0; i < nums.size() ; i++) {
-            if (i < nums.size() - 1 && nums[i] != 0 && nums[i] == nums[i+1]) {
-                result.push_back(nums[i] * 2);
-                i++;
-            } else if (nums[i] != 0) result.push_back(nums[i]);
-        }
-
-        while (nums.size() != result.size()) {
-            result.push_back(0);
-        }
-        return result;
-    }
+vector<int> applyOperations(vector<int>& nums) {vector<int> result;
+for (int i = 0; i < nums.size() ; i++) {if (i < nums.size() - 1 && nums[i] != 0 && nums[i] == nums[i+1]) {result.push_back(nums[i] * 2);i++;
+} else if (nums[i] != 0) result.push_back(nums[i]);}
+while (nums.size() != result.size()) {result.push_back(0);
+}
+return result;
+}
 };
