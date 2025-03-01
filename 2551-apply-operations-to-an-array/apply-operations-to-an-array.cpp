4bfd3a74e2b2f7f449f7234cc6848f07147af887
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-03-01 20:04:24 +0700

    Time: 0 ms (100.00%) | Memory: 13.1 MB (62.06%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,10 +1,15 @@
 class Solution {
-public:
-vector<int> applyOperations(vector<int>& nums) {vector<int> result;
-for (int i = 0; i < nums.size() ; i++) {if (i < nums.size() - 1 && nums[i] != 0 && nums[i] == nums[i+1]) {result.push_back(nums[i] * 2);i++;
-} else if (nums[i] != 0) result.push_back(nums[i]);}
-while (nums.size() != result.size()) {result.push_back(0);
-}
-return result;
-}
-};
+ public:
+ vector<int> applyOperations(vector<int>& nums) {int n=nums.size();
+ for(int i=0;i<n-1;i++){if(nums[i]==nums[i+1]){nums[i]=nums[i]*2;
+ nums[i+1]=0;
+ }
+ }
+ vector<int> ans;
+ for(int i=0;i<n;i++){
+ if(nums[i]!=0) ans.push_back(nums[i]);}
+ for(int i=0;i<n;i++){
+ if(nums[i]==0) ans.push_back(nums[i]);}
+ return ans;
+ }
+ };
