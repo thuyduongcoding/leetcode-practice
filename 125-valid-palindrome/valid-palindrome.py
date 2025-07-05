be0640f2a832091f4e1732160fba5399f076fa97
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-07-05 10:58:55 +0700

    Time: 8 ms (52.84%) | Memory: 23.2 MB (10.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,8 +4,6 @@ class Solution:
             return True
         sep = ""
         s_processed = sep.join([char.lower() for char in s if char.isalnum()])
-        len_s = len(s_processed)
-        for i in range(len_s//2):
-            if s_processed[i] != s_processed[len_s - 1 - i]:
-                return False
-        return True
+        if s_processed == s_processed[::-1]:
+            return True
+        return False
