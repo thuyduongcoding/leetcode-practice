ff582b5193c08330df64c667810ba8d8ba8fbfca
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-07-31 20:20:54 +0700

    Time: 51 ms (5.06%) | Memory: 22.9 MB (5.19%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def convert_word_to_tuple(word: str):
+            l = [0] * 26
+            for c in word:
+                l[ord(c) - ord('a')] +=1
+            return tuple(l)
+
+        char_list_map = {}
+
+        for word in strs:
+            word_count_tuple = convert_word_to_tuple(word)
+            print(word_count_tuple)
+
+            if word_count_tuple not in char_list_map:
+                char_list_map[word_count_tuple] = []
+            char_list_map[word_count_tuple].append(word)
+
+        result = []
+        for key, value in char_list_map.items():
+            result.append(value)
+
+        return result
+            
+        
