33e173d68c882fdcdd9d1f0b2a2992f869b981ae
Duong Nguyen <88173269+thuyduongcoding@users.noreply.github.com>
2025-07-31 20:23:06 +0700

    Time: 11 ms (82.53%) | Memory: 21.4 MB (40.26%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,26 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def convert_word_to_tuple(word: str):
-            # l = [0] * 26
-            # for c in word:
-            #     l[ord(c) - ord('a')] +=1
-            # return tuple(l)
-            return tuple(sorted(word))
+        groups = {}
+        res = []
 
-        char_list_map = {}
+        for s in strs:
+            sortedStr = tuple(sorted(s))
+            if tuple(sortedStr) not in groups:
+                groups[sortedStr] = []
+            groups[sortedStr].append(s)
 
-        for word in strs:
-            word_count_tuple = convert_word_to_tuple(word)
-            print(word_count_tuple)
+        for group in groups.values():
+            res.append(group)
 
-            if word_count_tuple not in char_list_map:
-                char_list_map[word_count_tuple] = []
-            char_list_map[word_count_tuple].append(word)
-
-        result = []
-        for key, value in char_list_map.items():
-            result.append(value)
-
-        return result
-            
-        
+        return res
